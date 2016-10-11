64223009208f7183cd9d8dd2067ae7b7b5b75102
Federico Bricker <fbricker@gmail.com>
2016-10-11 15:10:50 -0300

    Add scripts to enable and update more libraries

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 dir=`dirname "$0"`
 cd "$dir"
 rm -f extension-googleplayservices-basement.zip
-zip -0r extension-googleplayservices-basement.zip haxelib.json include.xml dependencies 
+zip -0r extension-googleplayservices-basement.zip haxelib.json include.xml dependencies template select_dependencies.sh update_dependencies.sh
